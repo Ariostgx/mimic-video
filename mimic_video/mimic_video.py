2cb3931a5b10d98474108e39671f919a75531678
lucidrains <lucidrains@gmail.com>
2026-01-12 14:53:44 -0800

    essential ingredient, still

--- mimic_video/mimic_video.py
+++ mimic_video/mimic_video.py
@@ -1,7 +1,11 @@
 import torch
-from torch.nn import Module, ModuleList
+from torch import nn
+from torch.nn import Module, ModuleList, Linear
 
 import torch.nn.functional as F
+
+from einops import einsum
+from einops.layers.torch import Rearrange
 
 # functions
 
@@ -10,6 +14,74 @@ def exists(v):
 
 def default(v, d):
     return v if exists(v) else d
+
+def divisible_by(num, den):
+    return (num % den) == 0
+
+# tensor function
+
+def max_neg_value(t):
+    return -torch.finfo(t.dtype).max
+
+# attention
+
+class Attention(Module):
+    def __init__(
+        self,
+        dim,
+        *,
+        dim_context = None,
+        dim_head = 64,
+        heads = 8,
+        kv_heads = 2
+    ):
+        super().__init__()
+        dim_q_inner = dim_head * heads
+        dim_kv_inner = dim_head * kv_heads
+        dim_context = default(dim_context, dim)
+
+        self.scale = dim_head ** -0.5
+
+        self.to_queries = Linear(dim, dim_q_inner, bias = False)
+        self.to_keys_values = Linear(dim_context, dim_kv_inner * 2, bias = False)
+        self.to_out = Linear(dim_q_inner, dim, bias = False)
+
+        assert divisible_by(heads, kv_heads)
+        groups = heads // kv_heads
+
+        self.split_q_heads = Rearrange('b n (g h d) -> b g h n d', g = groups, d = dim_head)
+        self.split_kv_heads = Rearrange('b n (h d) -> b h n d', d = dim_head)
+        self.merge_heads = Rearrange('b g h n d -> b n (g h d)')
+
+    def forward(
+        self,
+        tokens,
+        context = None,
+        context_mask = None
+    ):
+        context = default(context, tokens)
+
+        queries = self.to_queries(tokens)
+        keys, values = self.to_keys_values(context).chunk(2, dim = -1)
+
+        queries = self.split_q_heads(queries)
+        keys, values = tuple(self.split_kv_heads(t) for t in (keys, values))
+
+        queries = queries * self.scale
+
+        sim = einsum(queries, keys, 'b g h i d, b h j d -> b g h i j')
+
+        if exists(context_mask):
+            mask_value = max_neg_value(sim)
+            sim = einx.where('b j, b g h i j,', context_mask, sim, mask_value)
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum(attn, values, 'b g h i j, b h j d -> b g h i d')
+
+        out = self.merge_heads(out)
+
+        return self.to_out(out)
 
 # classes
 
